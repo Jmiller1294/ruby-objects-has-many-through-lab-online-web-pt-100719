7f51d6e04c31aca7248d993b1af928b522db32dd
Justin Miller <jayman1294@gmail.com>
2019-11-16 21:25:23 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre 
+
+  attr_accessor :name, :songs, :artists
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def songs 
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+  
+  def artists
+    self.all.collect do |song|
+      song.artist == self
+    end
+  end
+  
+  
+  def self.all 
+    @@all
+  end
+end
